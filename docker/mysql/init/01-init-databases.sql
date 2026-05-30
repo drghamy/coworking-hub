cc9483180895d0e0afef7c88ea21960a1ddbb2fb
drghamy <drghamy@users.noreply.github.com>
2026-05-30 13:19:34 +0300

    Separate booking and invoice databases

--- docker/mysql/init/01-init-databases.sql
+++ docker/mysql/init/01-init-databases.sql
@@ -1,9 +1,11 @@
 CREATE DATABASE IF NOT EXISTS coworking_db;
-CREATE DATABASE IF NOT EXISTS coworking_main_db;
+CREATE DATABASE IF NOT EXISTS coworking_booking_db;
+CREATE DATABASE IF NOT EXISTS coworking_invoice_db;
 
 CREATE USER IF NOT EXISTS 'springstudent'@'%' IDENTIFIED BY 'springstudent';
 
 GRANT ALL PRIVILEGES ON coworking_db.* TO 'springstudent'@'%';
-GRANT ALL PRIVILEGES ON coworking_main_db.* TO 'springstudent'@'%';
+GRANT ALL PRIVILEGES ON coworking_booking_db.* TO 'springstudent'@'%';
+GRANT ALL PRIVILEGES ON coworking_invoice_db.* TO 'springstudent'@'%';
 
 FLUSH PRIVILEGES;
